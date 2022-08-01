922950e591e47722b17dd0d39bfa363b5f054d89
peayah <82358003+peayah@users.noreply.github.com>
2022-08-01 14:56:35 -0700

    Add static() serve static files during development (only)

--- cohorts/urls.py
+++ cohorts/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
 path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
+
+# use static() to add URL mapping to serve
+# static files during development (only)
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
