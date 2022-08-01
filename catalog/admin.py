d484373b657882a02f6fc358dcb0f491d339906c
peayah <82358003+peayah@users.noreply.github.com>
2022-08-01 16:10:50 -0700

    import models into admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Leader, Language, Level
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Leader)
+admin.site.register(Language)
+admin.site.register(Level)
