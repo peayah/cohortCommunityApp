0132b97b7b68092bcd9d17f125351740bda553b8
peayah <82358003+peayah@users.noreply.github.com>
2022-08-01 15:48:27 -0700

    add event model

--- catalog/models.py
+++ catalog/models.py
@@ -1,18 +1,45 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Level(models.Model):
 
     level_type = models.CharField(max_length=200,
-                            help_text='Enter Level (Beginner, All, Specialized)')
+                                  help_text='Enter Level (Beginner, All, Specialized)')
 
     def __str__(self):
         return self.level_type
 
 
-class Laguage(models.Model):
+class Language(models.Model):
     language_type = models.CharField(max_length=200,
-                                  help_text='Enter Language (HTML, CSS, JavaScript, Data, API)')
+                                     help_text='Enter Language (HTML, CSS, '
+                                               'JavaScript, Data, API)')
 
     def __str__(self):
         return self.language_type
+
+
+class Event(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    date = models.DateField(null=True, blank=True)
+
+    # Foreign Key
+    leader = models.ForeignKey('Leader',
+                               on_delete=models.SET_NULL, null=True)
+
+    description = models.TextField(max_length=1000,
+                                   help_text='Enter a brief description of tevent')
+
+    level = models.ManyToManyField(Level,
+                                   help_text='Select a level for this event')
+
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):f
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('event-detail', args=[str(self.id)])
