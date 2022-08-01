0d6f87d686e9031a33fe9429f55a9b4ecde0114c
peayah <82358003+peayah@users.noreply.github.com>
2022-08-01 15:36:13 -0700

    add level Model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Level(models.Model):
+
+    level_type = models.CharField(max_length=200,
+                            help_text='Enter Level (Beginner, All, Specialized)')
+
+    def __str__(self):
+        return self.level_type
+
+
