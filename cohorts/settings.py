ae36abbba66da26a935ab455b92c0901d0798910
peayah <82358003+peayah@users.noreply.github.com>
2022-08-16 18:21:58 -0700

    add gitignore

--- cohorts/settings.py
+++ cohorts/settings.py
@@ -14,20 +14,24 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+import export as export
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t2x5j2mj^hc1qp_cxh$)kdvm*3y0lzvheq22%#tia003#)s52u'
+# SECRET_KEY = 'django-insecure-t2x5j2mj^hc1qp_cxh$)kdvm*3y0lzvheq22%#tia003#)s52u'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+                            'django-insecure-t2x5j2mj^hc1qp_cxh$)kdvm*3y0lzvheq22%#tia003#)s52u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cohorts.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -84,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -104,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -118,14 +119,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static'),
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -137,3 +135,4 @@ MEDIA_URL = '/media/'
 
 # Redirect to home URL after login
 LOGIN_REDIRECT_URL = '/'
+
