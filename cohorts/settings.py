787a257b48cf7be5f39c6fe29d7554d7f0b436b8
peayah <82358003+peayah@users.noreply.github.com>
2022-08-17 23:52:48 -0700

    add dotenv

--- cohorts/settings.py
+++ cohorts/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dotenv
-
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import export as export
